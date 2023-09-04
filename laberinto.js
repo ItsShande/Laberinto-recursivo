b3deb7c2600e07caa1ab9b1f564dee5985662f75
ItsShande <142055204+ItsShande@users.noreply.github.com>
2023-09-04 11:00:04 -0300

    Update laberinto.js

--- laberinto.js
+++ laberinto.js
@@ -1,120 +1,56 @@
-let laberintos = [
-    "S",
-    ".",
-    ".",
-    ".",
-    "#",
-    "#",
-    "#",
-    "#",
-    ".",
-    "#",
-    "#",
-    ".",
-    ".",
-    ".",
-    "#",
-    ".",
-    ".",
-    "#",
-    "#",
-    "#",
-    "E",
-    "#",
-    "#",
-    "#",
-    "#",
+const laberinto = [
+    ["#", "S", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", "#", "#", "#", "."],
+    [".", "#", "#", "#", "."],
+    [".", ".", ".", ".", "G"]
 ];
 
-let laberinto2 = [
-    "#",
-    "#",
-    "#",
-    "#",
-    "S",
-    "#",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    "#",
-    ".",
-    "#",
-    "#",
-    "#",
-    "#",
-    "#",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    "#",
-    "#",
-    "#",
-    "#",
-    "#",
-    ".",
-    "E",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-];
-
-function resolverLaberinto(laberinto, x, y, goalX, goalY) {
-    // Verificar si estamos fuera de los límites del laberinto o si estamos en una pared
-    if (
-        x < 0 ||
-        y < 0 ||
-        x >= laberinto.length ||
-        y >= laberinto[0].length ||
-        laberinto[x][y] === 1
-    ) {
+function label(y, x) {
+    if (y < 0 || y >= laberinto.length || x < 0 || x >= laberinto[y].length) {
         return false;
     }
 
-    // Verificar si hemos llegado a la posición objetivo
-    if (x === goalX && y === goalY) {
-        return true;
+    if (laberinto[y][x] === "G") {
+        return true; // llegamos al final
     }
 
-    // Marcar la celda actual como visitada
-    laberinto[x][y] = "visited";
-
-    // Intentar moverse en todas las direcciones posibles (arriba, abajo, izquierda, derecha)
-    if (
-        resolverLaberinto(laberinto, x - 1, y, goalX, goalY) ||
-        resolverLaberinto(laberinto, x + 1, y, goalX, goalY) ||
-        resolverLaberinto(laberinto, x, y - 1, goalX, goalY) ||
-        resolverLaberinto(laberinto, x, y + 1, goalX, goalY)
-    ) {
-        return true;
+    if (laberinto[y][x] === "#" || laberinto[y][x] === "+") {
+        return false; // si hay algun objeto enfrente o algun lugar que ya pasamos
     }
 
-    // Si ninguna de las direcciones conduce a la solución, retroceder
-    laberinto[x][y] = 0;
+    // Marcar el camino actual con +
+    laberinto[y][x] = "+";
+
+    // Buscar en todas las direcciones posibles
+    if (label(y - 1, x) || label(y, x - 1) || label(y + 1, x) || label(y, x + 1)) {
+        return true; // si encuentra una solucion en alguna direccion
+    }
+
+    // si no encuentra ningun camino se va para atras
+    laberinto[y][x] = ".";
     return false;
 }
 
-// Ejemplo de uso:
-const laberinto = [
-    [0, 1, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0],
-];
-
-const start = { x: 0, y: 0 };
-const goal = { x: 4, y: 4 };
-
-if (resolverLaberinto(laberinto, start.x, start.y, goal.x, goal.y)) {
-    console.log("Se encontró una solución.");
-    console.log(laberinto); // Laberinto resuelto
-} else {
-    console.log("No se encontró una solución.");
+// Encontrar la posición de inicio S
+let inicioY, inicioX;
+for (let i = 0; i < laberinto.length; i++) {
+    for (let j = 0; j < laberinto[i].length; j++) {
+        if (laberinto[i][j] === "S") {
+            inicioY = i;
+            inicioX = j;
+            break;
+        }
+    }
 }
+
+if (label(inicioY, inicioX)) {
+    console.log("Se encontró una ruta válida:");
+} else {
+    console.log("No se encontró una ruta válida.");
+}
+
+// muestra el laberinto con espacios 
+laberinto.forEach(row => {
+    console.log(row.join(" "));
+});
