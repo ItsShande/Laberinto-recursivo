c7707aefaabb0dc35aed94357f1780e6119a3eff
ItsShande <142055204+ItsShande@users.noreply.github.com>
2023-09-04 10:59:24 -0300

    Add files via upload

---
+++ trabajito.js
@@ -0,0 +1,17 @@
+let arreglo = [0, 1, 13, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let m = 9;
+
+//el coso este de aca hace que los valores del arreglo se ordenen de menor a mayor
+arreglo.sort(function (a, b) {
+    return a - b;
+});
+
+function EncontrarValor(x, i, f) {
+    //suma los valores del arreglo
+    f = arreglo.length
+    i = arreglo[0];
+    if(f + i / f === m) return true
+    if(m >)
+}
+
+console.log();
